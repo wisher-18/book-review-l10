8573fb932affd220ad8a706756f680856a6afd18
wisher-18 <johnnysonn10@gmail.com>
2024-03-07 23:49:21 +0530

    add database and some fake entries using factory

---
+++ app/Models/Book.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
+
+    public function scopeTitle(Builder $query, string $title):Builder{
+        return $query->where('title','LIKE','%'.$title.'%');
+    }
+}
