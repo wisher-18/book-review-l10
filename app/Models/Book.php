d25449658e0eae7a7204a2f736ebf47a9eef47ce
wisher-18 <johnnysonn10@gmail.com>
2024-03-08 02:10:18 +0530

    add views and controller for book for showing all books

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -17,4 +17,31 @@ class Book extends Model
     public function scopeTitle(Builder $query, string $title):Builder{
         return $query->where('title','LIKE','%'.$title.'%');
     }
+
+    public function scopePopular(Builder $query, $from = null, $to = null):Builder{
+        return $query->withCount(['reviews'=> fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)
+        ])
+        ->orderBy('reviews_count', 'desc');
+    }
+
+    public function scopeHighestRated(Builder $query, $from = null, $to = null):Builder{
+        return $query->withAvg(['reviews'=> fn(Builder $q) => $this->dateRangeFilter($q, $from, $to)], 'rating')
+            ->orderBy('reviews_avg_rating', 'desc');
+    }
+
+    private function dateRangeFilter(Builder $query, $from= null, $to = null){
+        if($from && !$to){
+            $query->where('created_at', '>=', $from);
+        }
+        elseif(!$from && $to){
+            $query->where('created_at', '<=', $to);
+        }
+        elseif($from && $to){
+            $query->whereBetween('created_at', [$from,$to]);
+        }
+    }
+
+    public function scopeMinReviews(Builder $query, int $minReviews):Builder{
+        return $query->having('reviews_count', '>=', $minReviews);
+    }
 }
